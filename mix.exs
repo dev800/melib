3d7e25b8f52d297034fbaaf86e01bc7830c8c49c
WangJinzhong <andywang7259@163.com>
2018-08-15 09:29:06 +0800

    up version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Melib.Mixfile do
     [
       app: :melib,
       name: "Melib",
-      version: "0.1.3",
+      version: "0.1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
